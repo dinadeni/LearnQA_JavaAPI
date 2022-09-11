2c04aa17c85cc48061ed92ebcfd063198f359a44
Dinara D <morella.ft@gmail.com>
2022-09-11 12:10:26 +0500

    HelloWorldTest

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,15 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld() {
-        System.out.println("Hello world");
+
+       Response response = RestAssured
+               .get("https://playground.learnqa.ru/api/get_text")
+               .andReturn();
+       response.print();
     }
 }
